a3601524083353c0b3dc533319bf7eb3eb441f89
jinchao <jinchaophp@126.com>
2022-04-28 17:41:45 +0800

    :sparkles: 通过 main 创建分支

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-import { getBranches } from "../src/request/index"
+import { createBranch, getBranches } from "../src/request/index"
 import { getElement, parseBranches } from "./utils/util"
 
-getBranches().then(res => {
-  const $branches = getElement("branches")
-  $branches.innerHTML = parseBranches(res)
+getBranchesFn()
+
+const $inputBranchName = getElement("inputBranchName")
+const $btnBranch = getElement("btnBranch")
+$btnBranch.addEventListener("click", async () => {
+  if ($inputBranchName.value) {
+    const result = await createBranch({
+      branchName: $inputBranchName.value
+    })
+    if (result) {
+      alert("创建分支成功")
+      getBranchesFn()
+      $inputBranchName.value = ""
+    } else {
+      alert("创建分支失败")
+    }
+  }
 })
+
+function getBranchesFn() {
+  getBranches().then(res => {
+    const $branches = getElement("branches")
+    $branches.innerHTML = parseBranches(res)
+  })
+}
--- src/request/index.js
+++ src/request/index.js
@@ -5,16 +5,23 @@ const octokit = new Octokit({
   auth: accessToken
 })
 
+const defaultHeader = {
+  headers: {
+    accept: "application/vnd.github.v3+json"
+  }
+}
+const defaultData = {
+  ...defaultHeader,
+  owner: "jinchao1992",
+  repo: "request-github-api"
+}
+
 const getBranches = async () => {
   try {
     const result = await octokit.request(
       "GET /repos/{owner}/{repo}/branches",
       {
-        headers: {
-          accept: "application/vnd.github.v3+json"
-        },
-        owner: "jinchao1992",
-        repo: "request-github-api",
+        ...defaultData,
         mediaType: {
           format: "raw"
         }
@@ -26,6 +33,43 @@ const getBranches = async () => {
   }
 }
 
+const getCurrentCommit = async () => {
+  const result = await octokit.request(
+    "GET /repos/{owner}/{repo}/commits",
+    {
+      ...defaultData,
+      per_page: 1
+    }
+  )
+  return result.data[0]
+}
+
+const createRef = async (params) => {
+  const { branchName, commit } = params
+  return await octokit.request(
+    "POST /repos/{owner}/{repo}/git/refs",
+    {
+      ...defaultData,
+      ref: `refs/heads/${branchName}`,
+      sha: commit
+    }
+  )
+}
+
+const createBranch = async (params) => {
+  try {
+    const commit = await getCurrentCommit()
+    return await createRef({
+      ...params,
+      commit: commit.sha
+    })
+  } catch (e) {
+    console.log(e)
+    return null
+  }
+}
+
 export {
-  getBranches
+  getBranches,
+  createBranch
 }
