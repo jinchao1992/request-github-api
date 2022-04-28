237f08ef2ff252d2239b4c146689464103fa42bf
jinchao <jinchaophp@126.com>
2022-04-28 15:05:20 +0800

    :sparkles: 获取 repo 相关信息

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log(123)
+import { Octokit } from "@octokit/core"
+
+const assessToken = "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"
+const octokit = new Octokit({
+  auth: assessToken
+})
+const result = await octokit.request(
+  "GET /repos/{owner}/{repo}",
+  {
+    owner: "jinchao1992",
+    repo: "request-github-api",
+    mediaType: {
+      format: "raw"
+    }
+  }
+)
+
+console.log(result.data)
